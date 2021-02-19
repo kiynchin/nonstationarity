02b6ca19007c2deab615852c36685a94693c7e27
Keene Chin <keene.chin@gmail.com>
2021-02-19 12:35:22 -0500

    more readable parallel structure in loop

--- pendulum.py
+++ pendulum.py
@@ -26,8 +26,9 @@ class Dynamics:
 
 
 def plot_comparison(field: str):
-    plt.plot(np.linspace(0, T, N+1), traj[field], c='k')
-    plt.scatter(np.linspace(2*dt, T+dt, N), neural_traj[field], c='b')
+    plt.figure()
+    plt.plot(np.linspace(0, T, N), traj[field], c='k')
+    plt.scatter(np.linspace(0, T, N), neural_traj[field], c='b')
     plt.xlabel("Time (s)")
     plt.ylabel(field)
     plt.legend(["ground_truth", "neural_predictor"])
@@ -41,26 +42,39 @@ if __name__ == "__main__":
     T = 10
     N = int(T/dt)
 
-    x = np.array((np.pi/2, 0), dtype=State)
-    traj = np.empty((N+1,), dtype=State)
-    traj[0] = x
+    u0 = 1
+    u_traj = [u0*random.random()-0.5 for i in range(N)]
 
-    neural_traj = np.empty((N,), dtype=State)
+    x0 = np.array((np.pi/2, np.pi/2), dtype=State)
+    traj = np.empty((N,), dtype=State)
+    traj[0] = x0
+    x1 = dyn(x0, u_traj[0])
+    traj[1] = x1
+    
     neural_network = MLPRegressor(random_state=1, max_iter=500)
+    neural_traj = np.empty((N,), dtype=State)
+    neural_traj[0] = None
+    neural_traj[1] = None
+    neural_network.partial_fit(X=np.array([x0['theta'], x0['theta_dot'], u_traj[0]]).reshape(1, -1),
+                               y=np.array([x1['theta'], x1['theta_dot']]).reshape(1, -1))
 
-    u = 0
-    u0 = 2
-    for i in range(N):
-        if i%int(N/N) == 0:
-            u = u0*(random.random()-0.5)
+    error_traj = []
+    control_epoch_length = N
+    x = x1
+    u = u_traj[0]
+    for i in range(2, N):
+        if i % int(N/control_epoch_length) == 0:
+            u = u_traj[i]
         x_new = dyn(x, u)
-        traj[i+1] = x_new
-
-        neural_network.partial_fit(np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1),
-                                   np.array([x_new['theta'], x_new['theta_dot']]).reshape(1, -1))
-        x_neural = neural_network.predict(np.array([x_new['theta'], x_new['theta_dot'], u]).reshape(1, -1))
+        traj[i] = x_new
+        x_neural = neural_network.predict(np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1))
         x_neural = np.array((x_neural[0, 0], x_neural[0, 1]), dtype=State)
         neural_traj[i] = x_neural
+
+        error = (x_neural[field]-x_new[field] for field in ['theta', 'theta_dot'])
+        error_traj.append(error)
+        neural_network.partial_fit(X=np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1),
+                                   y=np.array([x_new['theta'], x_new['theta_dot']]).reshape(1, -1))
         x = x_new
     
     plot_comparison('theta')
