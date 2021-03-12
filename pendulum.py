51f348fbc840fccdf5d20da0fa4c45cc84c99b6d
Keene Chin <keene.chin@gmail.com>
2021-03-12 08:42:31 -0500

    broken oop

--- pendulum.py
+++ pendulum.py
@@ -6,20 +6,19 @@ from sklearn.neural_network import MLPRegressor
 PendulumPhase = np.dtype([('theta', 'f8'), ('theta_dot', 'f8')])
 
 
-class Dynamics:
-    def __init__(self, m, g, l, b, dt):
+class PendulumDynamics:
+    def __init__(self, m, g, length, b):
         self.m = m
         self.g = g
-        self.l = l
+        self.length = length
         self.b = b
-        self.dt = dt
 
-    def __call__(self, x:PendulumPhase, u):
+    def __call__(self, x: PendulumPhase, u):
         m = self.m
         g = self.g
-        l = self.l
+        length = self.length
         b = self.b
-        alpha = u/(m*l**2) - (g/l)*np.sin(x['theta'])-b*x['theta_dot']
+        alpha = u/(m*length**2) - (g/length)*np.sin(x['theta'])-b*x['theta_dot']
         theta_new = x['theta']+x['theta_dot']*self.dt
         theta_dot_new = x['theta_dot'] + alpha*self.dt
         return np.array((theta_new, theta_dot_new), dtype=PendulumPhase)
@@ -28,26 +27,57 @@ class Dynamics:
         self.b = random.random()
 
 
-def plot_comparison(field: str):
-    plt.figure()
-    plt.plot(np.linspace(0, T, N), traj[field], c='k')
-    plt.scatter(np.linspace(0, T, N), predicted_traj[field], c='b')
 
-    plt.xlabel("Time (s)")
-    plt.ylabel(field)
-    plt.legend(["Ground Truth", "Model Prediction"])
-    plt.title(f"Tracking performance of {field}")
-    plt.show()
+class Env:
+    def __init__(self, dyn, T=15, dt=0.01, control_epochs=1, dynamics_epochs=10):
+        self.dynamics = dyn
+        self.dt = dt
+        self.N = int(T/dt)
+        self.num_control_epochs = control_epochs
+        self.num_dynamics_epochs = dynamics_epochs
+        self.control_epoch_length = int(self.N/self.num_control_epochs)
+        self.dynamics_epoch_length = int(self.N/self.num_dynamics_epochs)
 
-    plt.figure()
-    plt.plot(np.linspace(0, T, N), np.abs(error_traj[field]), c='r')
-    for i in range(1, num_dynamics_epochs):
-        plt.axvline(T*i/num_dynamics_epochs)
-    plt.xlabel("Time (s)")
-    plt.ylabel("Prediction % Error")
-    plt.title(f"Error in {field} vs. Dynamics Changes")
-    plt.legend(["Error", "Dynamics Shift"])
-    plt.show()
+    def run_experiment(self, x):
+        for i in range(2, self.N):
+            if i % self.control_epoch_length == 0:
+                u = u_traj[i]
+
+            if i % self.dynamics_epoch_length == 0:
+                self.dyn.update()
+                
+            xnew_actual = dyn(x, u, env.dt)
+            traj[i] = xnew_actual
+            xnew_predicted = model_learner.predict(np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1))
+            xnew_predicted = np.array((xnew_predicted[0, 0], xnew_predicted[0, 1]), dtype=PendulumPhase)
+            predicted_traj[i] = xnew_predicted
+            error = ((xnew_predicted['theta']-xnew_actual['theta'])/xnew_predicted['theta'],
+                    (xnew_predicted['theta_dot']-xnew_actual['theta_dot'])/xnew_predicted['theta_dot'])
+            error_traj[i] = np.array(error, dtype=PendulumPhase)
+            model_learner.partial_fit(X=np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1),
+                                    y=np.array([xnew_actual['theta'], xnew_actual['theta_dot']]).reshape(1, -1))
+            x = xnew_actual
+    
+    def plot_comparison(self, field: str):
+        plt.figure()
+        plt.plot(np.linspace(0, self.T, self.N), self.traj[field], c='k')
+        plt.scatter(np.linspace(0, self.T, self.N), self.predicted_traj[field], c='b')
+
+        plt.xlabel("Time (s)")
+        plt.ylabel(field)
+        plt.legend(["Ground Truth", "Model Prediction"])
+        plt.title(f"Tracking performance of {field}")
+        plt.show()
+
+        plt.figure()
+        plt.plot(np.linspace(0, self.T, self.N), np.abs(error_traj[field]), c='r')
+        for i in range(1, self.num_dynamics_epochs):
+            plt.axvline(self.T*i/self.num_dynamics_epochs)
+        plt.xlabel("Time (s)")
+        plt.ylabel("Prediction % Error")
+        plt.title(f"Error in {field} vs. Dynamics Changes")
+        plt.legend(["Error", "Dynamics Shift"])
+        plt.show()
 
 
 
@@ -57,18 +87,15 @@ class ModelLearner:
 
 
 if __name__ == "__main__":
-    dt = 0.01
-    dyn = Dynamics(m=5, g=9.81, l=2, b=0.5, dt=dt)
-    T = 15
-
-
-    N = int(T/dt)
+    dyn = PendulumDynamics(m=5, g=9.81, length=2, b=0.5)
+    env = Env(dyn, T=15, dt=0.01)
 
     u_scale = 5
-    u_traj = [u_scale*random.random()-0.5 for i in range(N)]
+    u_traj = [u_scale*random.random()-0.5 for i in range(env.N)]
     u = u_traj[0]
     theta_0 = np.pi/2
     theta_dot_0 = 0
+
     x0 = np.array((theta_0, theta_dot_0), dtype=PendulumPhase)
     traj = np.empty((N,), dtype=PendulumPhase)
     traj[0] = x0
@@ -84,34 +111,10 @@ if __name__ == "__main__":
 
     error_traj = np.empty((N,), dtype=PendulumPhase)
     error_traj[0], error_traj[1] = (None, None)
-    num_control_epochs = 1
-    num_dynamics_epochs = 10
 
-    control_epoch_length = int(N/num_control_epochs)
-    dynamics_epoch_length = int(N/num_dynamics_epochs)
-    x = x1
-
-    for i in range(2, N):
-        if i % control_epoch_length == 0:
-            u = u_traj[i]
-
-        if i % dynamics_epoch_length == 0:
-            dyn.update()
-            
-        xnew_actual = dyn(x, u)
-        traj[i] = xnew_actual
-        xnew_predicted = model_learner.predict(np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1))
-        xnew_predicted = np.array((xnew_predicted[0, 0], xnew_predicted[0, 1]), dtype=PendulumPhase)
-        predicted_traj[i] = xnew_predicted
-        error = ((xnew_predicted['theta']-xnew_actual['theta'])/xnew_predicted['theta'],
-                 (xnew_predicted['theta_dot']-xnew_actual['theta_dot'])/xnew_predicted['theta_dot'])
-        error_traj[i] = np.array(error, dtype=PendulumPhase)
-        model_learner.partial_fit(X=np.array([x['theta'], x['theta_dot'], u]).reshape(1, -1),
-                                   y=np.array([xnew_actual['theta'], xnew_actual['theta_dot']]).reshape(1, -1))
-        x = xnew_actual
-    
-    plot_comparison('theta')
-    plot_comparison('theta_dot')
+    env.run_experiment(x1)
+   
+    env.plot_comparison('theta')
+    env.plot_comparison('theta_dot')
         
-
 
